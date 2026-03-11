8f6b2e6f6810977b321fb330c457681575e45def
Dev7ex <46531389+Dev7ex@users.noreply.github.com>
2026-03-11 15:54:18 +0100

    Create MultiCommonPlugin.java

---
+++ multicommon-paper/src/main/java/com/dev7ex/multicommon/MultiCommonPlugin.java
@@ -0,0 +1,12 @@
+package com.dev7ex.multicommon;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+
+/**
+ * @author Dev7ex
+ * @since 24.05.2025
+ */
+@Getter(AccessLevel.PUBLIC)
+public class MultiCommonPlugin {
+}
